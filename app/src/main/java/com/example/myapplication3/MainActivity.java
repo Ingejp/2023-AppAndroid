c9e9d4f7ebf15adb6dce24890418282a2adafabb
JP <ingejp@outlook.com>
2023-07-28 21:55:41 -0600

    Primera clase, navegación básica entre pantallas

---
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        Intent intent = null;
+        intent = new Intent(this, MainActivity2.class );
+
+        if (intent!=null){
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication3/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.myapplication3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void onClick(View view){
+        Intent intent = null;
+        intent = new Intent(this, MainActivity.class );
+
+        if (intent!=null){
+            startActivity(intent);
+        }
+    }
+}
